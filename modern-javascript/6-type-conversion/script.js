c847a71f00ffe7ccf004142cd91acd053bf12bfb
jahnav-sannapureddy <jahnavsannapureddy@gmail.com>
2023-12-14 20:58:25 +0530

    added type conver-1

---
+++ modern-javascript/6-type-conversion/script.js
@@ -0,0 +1,22 @@
+let num = '37';
+console.log(num, typeof num);
+
+// string to number (int)
+console.log(parseInt(num));
+console.log(+num);
+console.log(Number(num));
+
+// number to string
+num = 23
+console.log(num, typeof num);
+console.log(num.toString(), typeof num.toString());
+console.log(String(num), typeof String(num));
+
+num = '88.12';
+console.log(num, typeof num);
+
+// string to float
+console.log(parseFloat(num), typeof parseFloat(num));
+
+
+// boolean
